1853dbfd3e6b0e071723c2c30f74374cebf59d30
Shrigiri014 <karanshrigiri020@gmail.com>
2025-05-23 22:14:38 +0530

    Create 3c_IsNumeric.java

---
+++ 3c_StringExercise2/3c_IsNumeric.java
@@ -0,0 +1,15 @@
+/*  3c. String Exercise progams
+    Q8. Write a Java Program for Verifying if a string contains only numeric characters using user defined function isNumeric().
+*/
+
+
+public class IsNumeric {
+    public static boolean isNumeric(String str) {
+        return str != null && str.matches("\\d+");
+    }
+
+    public static void main(String[] args) {
+        String input = "123456";
+        System.out.println("Is numeric? " + isNumeric(input));
+    }
+}

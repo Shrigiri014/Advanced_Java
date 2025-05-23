958776c06145fef6dc0e81dfb7b306dcfde86b71
Shrigiri014 <karanshrigiri020@gmail.com>
2025-05-23 22:10:58 +0530

    Create 3b_ReverseString.java

---
+++ 3b_StringExercise1/3b_ReverseString.java
@@ -0,0 +1,15 @@
+/* 3b. String Exercise progams
+   Q3. Write a Java Program for Reversing the characters in a string using user defined function reverseString(). 
+*/
+
+
+public class ReverseString {
+    public static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "AIET";
+        System.out.println("Reversed: " + reverseString(input));
+    }
+}

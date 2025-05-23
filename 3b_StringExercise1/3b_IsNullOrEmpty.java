fb2a2355f56eb6e139c59ba1b3f35d4c200769ff
Shrigiri014 <karanshrigiri020@gmail.com>
2025-05-23 22:07:40 +0530

    Create 3b_IsNullOrEmpty.java

---
+++ 3b_StringExercise1/3b_IsNullOrEmpty.java
@@ -0,0 +1,15 @@
+/* 3b. String Exercise progams
+   Q1. Write a Java Program for Checking if a given string is null or contains only whitespace using user defined function isNullOrEmpty().
+*/
+
+
+public class IsNullOrEmpty {
+    public static boolean isNullOrEmpty(String str) {
+        return str == null || str.trim().isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String input = "   ";
+        System.out.println("Is null or empty? " + isNullOrEmpty(input));
+    }
+}
